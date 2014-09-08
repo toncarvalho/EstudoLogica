a5eb38c84257839a9a16337d0a89a1d6f1a32e62
Ton Carvalho <wellington.carvalho@atualcs.com.br>
2014-09-08 17:32:34 -0300

    Implementação de primeiro exemplo com matrizes.

---
+++ src/main/java/estudos/matrizes/ExemploMatriz.java
@@ -0,0 +1,41 @@
+package estudos.matrizes;
+
+/**
+ * Uso de Arrays como tabelas de dados em memoria
+ */
+public class ExemploMatriz {
+    private int LINHAS = 2;
+    private int COLUNAS = 2;
+
+    private double[][] matriz = new double[LINHAS][COLUNAS];
+
+    /**
+     * Imagine uma linha de registro( Tupla) com os campos nome, endereco e email, como conter estes dados em memoria? -> o campo nome pode
+     * contem ate 10 caracteres, o endereco 15 e o email tambem 15.
+     */
+
+    /**
+     * implementacao de uma matriz que contenha alunos e notas
+     */
+
+    public void carregarMemoria() {
+
+        //aluno 1
+        matriz[0][0] = 1;
+        //nota 89
+        matriz[0][1] = 89;
+        //aluno 2
+        matriz[1][0] = 2;
+        //nota 89
+        matriz[1][1] = 58;
+    }
+
+    public void imprime() {
+
+        for (int linha = 0; linha < LINHAS; linha++) {
+            for (int coluna = 0; coluna < COLUNAS; coluna++) {
+                System.out.println("Aluno: " + matriz[linha][coluna]);
+            }
+        }
+    }
+}
---
+++ src/test/java/TesteExemploMatriz.java
@@ -0,0 +1,16 @@
+import estudos.matrizes.ExemploMatriz;
+import org.junit.Test;
+
+/**
+ * Created by ton on 9/8/14.
+ */
+public class TesteExemploMatriz {
+
+    @Test
+    public void testaMatriz() {
+        ExemploMatriz matriz = new ExemploMatriz();
+        matriz.carregarMemoria();
+        matriz.imprime();
+
+    }
+}
